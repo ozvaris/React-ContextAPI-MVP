3cd24e3e759e005fdfb13b25f71aae6b8a12874d
Ozgur <ozgurozvaris@hotmail.com>
2022-03-14 09:57:10 +0000

    firebase url updated

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,22 +1,22 @@
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 
-import NewMeetupForm from '../components/meetups/NewMeetupForm';
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
   const history = useHistory();
 
   function addMeetupHandler(meetupData) {
     fetch(
-      'https://react-getting-started-48dec-default-rtdb.firebaseio.com/meetups.json',
+      "https://react-meetup-c148a-default-rtdb.firebaseio.com/meetups.json",
       {
-        method: 'POST',
+        method: "POST",
         body: JSON.stringify(meetupData),
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
       }
     ).then(() => {
-      history.replace('/');
+      history.replace("/");
     });
   }
 
